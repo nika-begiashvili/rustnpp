fe136b7dbb5d3a28aba1f4c057e381d438bba92b
ნიკა <nika.begiashvili@gmail.com>
2016-04-26 23:13:05 +0400

    gitignore changes

--- src/functions.rs
+++ src/functions.rs
@@ -3,8 +3,8 @@ use helpers;
 use std::path::Path;
 use std::env;
 
-fn cargoProject( s : String, cmd: String ) -> bool{
-	let mut p = Some( Path::new(&s) );
+fn cargoProject( currentFile : &String, cmd: String ) -> bool{
+	let mut p = Some( Path::new(&currentFile) );
 	while p.unwrap().parent() != None{
 		
 		if p.unwrap().file_name().unwrap() == "src"{
@@ -23,14 +23,40 @@ fn cargoProject( s : String, cmd: String ) -> bool{
 	false
 }
 
+fn rustFile( currentFile :&String, run :bool ) -> bool{
+	let p = Path::new(&currentFile);
+	let fname = p.file_name().unwrap();
+	let fstem = p.file_stem().unwrap();
+	let par = p.parent().unwrap();
+	let cwd = env::current_dir();
+	
+	let mut cmd :String = String::new();
+	if run{
+		cmd = format!(" & {}", fstem.to_str().unwrap() );
+	}
+	env::set_current_dir( par ).unwrap();
+	helpers::launchCmdWindow( format!("rustc {} {}", fname.to_str().unwrap() , cmd) );
+	env::set_current_dir( cwd.unwrap() ).unwrap();
+	
+	return true;
+}
+
+
+
 //  exposed functions
 
 pub extern "C" fn runProgram(){
 	let s = helpers::getCurrentNppFile();
-	cargoProject( s, String::from("run") );
+	if !cargoProject( &s, String::from("run") ){
+		rustFile(&s,true);
+	}
 }
 
 pub extern "C" fn buildProgram(){
 	let s = helpers::getCurrentNppFile();
-	cargoProject( s, String::from("build") );
+	if !cargoProject( &s, String::from("build") ){
+		rustFile(&s,false);
+	}
 }
+
+
